6376378e0e24a572d723eab08223570d525358c2
Mario <molinamario.msc@gmail.com>
2022-06-14 13:52:24 +0200

    Versión con try

--- PruebaGrupoJava.java
+++ PruebaGrupoJava.java
@@ -1,30 +1,35 @@
 package pruebagrupojava;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class PruebaGrupoJava {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int num, repetic;
+    try (Scanner sc = new Scanner(System.in)){
+      int num, repetic;
     
-    System.out.print("Escribe un numero entero: ");
-    num = sc.nextInt();
-    num=(int)Math.pow(num, 2);
-    System.out.println("su cuadrado es "+num);
-    System.out.print("¿Cuantas veces deseas que se imprima?: ");
-    repetic = sc.nextInt();
-    
-    for(int i=0; i<repetic; i++){
-      System.out.printf("%d. %d\n",i+1, num);
-      if(i%2==0){
-          System.out.println("es la ronda par");
+      System.out.print("Escribe un numero entero: ");
+      num = sc.nextInt();
+      //num=Integer.parseInt(JOptionPane.showInputDialog("Escribe un numero entero: "));
+      num=(int)Math.pow(num, 2);
+      System.out.println("su cuadrado es "+num);
+      System.out.print("¿Cuantas veces deseas que se imprima?: ");
+      repetic = sc.nextInt();
+      //repetic=Integer.parseInt(JOptionPane.showInputDialog("¿Cuantas veces deseas que se imprima?: "));
+
+      for(int i=0; i<repetic; i++){
+        System.out.printf("%d. %d\n",i+1, num);
+        if(i%2==0){
+            System.out.println("es la ronda par");
+        }
+        else{
+            System.out.println("es la ronda impar");
+        }
       }
-      else{
-          System.out.println("es la ronda impar");
-      }
+      System.out.println("");
+    } catch (Exception e) {
     }
-    System.out.println("");
-    
+
   }
 }
